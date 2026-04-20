91c3cecaa2808cacb5ac095d29fa403734de299f
Faizul <fazzu5530@gmail.com>
2026-04-20 15:59:29 +0530

    added dockerfile

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using PortfolioApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://0.0.0.0:10000");
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
